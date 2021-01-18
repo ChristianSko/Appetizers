b409b4980f8f7f7b128bd9664011769cc1746b3d
Christian Skorobogatow <christiansko@icloud.com>
2021-01-18 10:36:54 +0100

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Christian Skorobogatow on 18/1/21.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
