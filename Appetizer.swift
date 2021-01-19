51071a9b5e2152fc142f237a7890116d887d7954
Christian Skorobogatow <christiansko@icloud.com>
2021-01-19 11:52:45 +0100

    created appetizer model and a corresponding mock data for development

---
+++ Appetizer.swift
@@ -0,0 +1,35 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by Christian Skorobogatow on 19/1/21.
+//
+
+import Foundation
+
+struct Appetizer: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+
+struct MockData {
+    static let sampleAppetizer = Appetizer(id: 0001,
+                                           name: "Test Appetizer",
+                                           description: "This is a description for my appetizer",
+                                           price: 9.99,
+                                           imageURL: "",
+                                           calories: 99,
+                                           protein: 99,
+                                           carbs: 99)
+}
