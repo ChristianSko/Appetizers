7440d28e247c6742d64a912152f8b7ed283d616f
Christian Skorobogatow <christiansko@icloud.com>
2021-01-21 10:57:00 +0100

    adding network call

---
+++ Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Christian Skorobogatow on 21/1/21.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
