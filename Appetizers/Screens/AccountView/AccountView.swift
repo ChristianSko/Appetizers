72a84c4b69e4fa021c82b0e78c71106e07ea4339
Christian Skorobogatow <christiansko@icloud.com>
2021-01-28 10:53:48 +0100

    added base UI for AccountView

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -8,10 +8,41 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lasttName = ""
+    @State private var email = ""
+    @State private var birthday = Date()
+    @State private var napkins = false
+    @State private var frequentRefills = false
+    
     var body: some View {
         NavigationView{
-            Text("AccountView")
-                .navigationTitle("👌Account")
+            Form{
+                Section(header: Text("Personal Info")) {
+    
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lasttName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                    
+                    DatePicker("Birthday", selection: $birthday, displayedComponents: .date)
+                    Button {
+                        print("save")
+                    } label: {
+                        Text("Save changes")
+                    }
+                }
+                
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkins", isOn: $napkins)
+                    Toggle("Frequent refills", isOn: $frequentRefills)
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+            }
+            .navigationTitle("👌Account")
         }
     }
     
